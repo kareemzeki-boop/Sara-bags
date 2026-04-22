78305f1310eadffa0fe0b0c29ca97e12d6c3ecae
Ze13ki <kareemzeki@gmail.com>
2026-04-22 16:21:17 +0400

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./database');
+const db = require('../db/database');
 
 // GET /api/products
 router.get('/', (req, res) => {
