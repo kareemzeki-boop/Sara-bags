aafccef7359e41a74fbc365c6664d735ec9830fd
Ze13ki <kareemzeki@gmail.com>
2026-04-22 15:55:41 +0400

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const db = require('../db/database');
+const db = require('./database');
 
 // Generate order reference
 const generateRef = () => {
